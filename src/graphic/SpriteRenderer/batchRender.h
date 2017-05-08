be5bbe1e95987aecb15c779bd8e725d75d430967
DovahChief <pepelimonta@gmail.com>
2017-05-07 22:28:22 -0500

    batch renderer

--- src/graphic/SpriteRenderer/batchRender.h
+++ src/graphic/SpriteRenderer/batchRender.h
@@ -11,7 +11,7 @@
 namespace spark {
     namespace graphics {
 
-        #define RENDERER_MAX_SPRITES    60000
+        #define RENDERER_MAX_SPRITES    10000
         #define RENDERER_VERTEX_SIZE    sizeof(VertexData)
         #define RENDERER_SPRITE_SIZE    RENDERER_VERTEX_SIZE * 4
         #define RENDERER_BUFFER_SIZE    RENDERER_SPRITE_SIZE * RENDERER_MAX_SPRITES
