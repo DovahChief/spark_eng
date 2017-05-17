0a5b1fe7f35d53bee074c473580b1207932ebdbb
DovahChief <pepelimonta@gmail.com>
2017-05-16 22:07:05 -0500

    reorganizacion menor

--- src/spark.cpp
+++ src/spark.cpp
@@ -5,7 +5,7 @@
  *
  * */
 
-#include "src/includes.h"
+#include "includes.h"
 
 int main(){
 
@@ -14,19 +14,26 @@ int main(){
 	using namespace math;
 
 	window w("SPARK2", 800, 450);
-    shader shad("/home/jose/Documentos/c++/eclipseWS/spark/src/shader/basicShader.vert",
-				"/home/jose/Documentos/c++/eclipseWS/spark/src/shader/basicShader.frag");
-	shad.enable();
+	shader* shad = new shader { "/home/jose/Documentos/c++/eclipseWS/spark/src/shader/basicShader.vert",
+			"/home/jose/Documentos/c++/eclipseWS/spark/src/shader/basicShader.frag" };
+	shader* shad2 = new shader { "/home/jose/Documentos/c++/eclipseWS/spark/src/shader/basicShader.vert",
+			"/home/jose/Documentos/c++/eclipseWS/spark/src/shader/basicShader.frag" };
+
+
 	//mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
 	//shad.setUniformMAT4("pr_mat", ortho);
     
-    tileLayer layer1(&shad);
+    tileLayer layer1(shad);
+    tileLayer layer2(shad2);
+
     srand(time(NULL));
     for (float y = -9.0f; y < 9.0f; y += 0.1){
         for (float x = -16.0f; x < 16.0f; x += 0.1){
             layer1.add( new sprite {x,y, 0.09f, 0.09f, vec4(0,0,rand()%1000/1000.0f,0)});
         }
     }
+
+    layer2.add(new sprite {0,0, 4.0f, 4.0f, vec4(0.7,0,0,0)});
 
 	timer tm { };
     unsigned short fps {0};
@@ -36,10 +43,18 @@ int main(){
         w.clear();
 		double x {}, y {};
 		w.mouse_position(x, y);
-		shad.setUniform2f("light_pos", vec2((float) ( (x * 32.0f/800.0f) - 16.0f ),
+        shad->enable();
+		shad->setUniform2f("light_pos", vec2((float) ( (x * 32.0f/800.0f) - 16.0f ),
                                              (float) (9.0f-(y * 18.0f/450.0f)) ));
 
+        //shad->setUniform2f("light_pos", vec2(0,4));
+        shad2->enable();
+        shad2->setUniform2f("light_pos", vec2((float) ( (x * 32.0f/800.0f) - 16.0f ),
+                                            (float) (9.0f-(y * 18.0f/450.0f)) ));
+       shad->setUniformMAT4("ml_mat",mat4::rotation(30.0f, vec3(0,1,0)));
+        layer2.render();
         layer1.render();
+
 		w.update();
 
         fps++;
@@ -50,6 +65,6 @@ int main(){
         }
 
 	}
-    shad.disable();
+
 	return (0);
 }
